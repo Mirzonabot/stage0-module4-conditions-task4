7d341977b919fcda9bc8be8cd30ef258e3190a60
Mirzonabot <mirzonabot.mirzonabotov_2023@ucentralasia.org>
2023-07-17 23:34:40 +0600

    solved

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+//        Implement the program that will consume 3 numbers from method arguments
+//        and it will print the print greatest out of three. In case if there are
+//        more than 1 biggest -> print any of them.(Libraries are forbidden)
+        System.out.println(first > second ? (first > third ? first : third) : (second > third ? second : third));
     }
 }
